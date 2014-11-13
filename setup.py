0d9d2c905c8ebff023b3889933a098d087c3877c
christopher <cfricke@gisinc.com>
2014-11-13 12:37:26 -0500

    Add licensing.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name = 'ArcPyLogging',
-    version = '1',
-    packages = ['ArcPyLogger'],
-    url = '',
-    license = '',
-    author = 'cfricke and carnold',
-    author_email = '',
-    description = 'Classes to define ArcPy message stream handlers and a helper function for us noobs.'
+    name='ArcPyLogging',
+    version='1.0.0',
+    packages=['ArcPyLogger'],
+    url='https://git.gisinc.com/projects/GITHUB/repos/arcpylogger',
+    license='MIT',
+    author='cfricke, carnold',
+    author_email='cfricke@gisinc.com, carnold@gisinc.com',
+    description='Classes to define ArcPy message stream handlers and a helper function for us noobs.'
 )
