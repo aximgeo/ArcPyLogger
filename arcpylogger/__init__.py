4bdd0617b0ca39833cbe58e2505f99b37aff327c
christopher <cfricke@gisinc.com>
2015-03-16 16:24:12 -0400

    follow pep 8 naming conventions

--- arcpylogger/__init__.py
+++ arcpylogger/__init__.py
@@ -1,13 +1,13 @@
-"""
+__doc__ = """
 Title: ArcPy Logger
 Description: Classes to define ArcPy message stream handlers and a helper function for us noobs.
 Usage:
 """
-
-__author__ = 'cfricke'
-__version__ = '0.9'
-__maintainer__ = 'cfricke'
+__author__ = 'Charles Arnold (carnold@gisinc.com) and Christopher Fricke (cfricke@gisinc.com)'
+__version__ = '1.0.0'
 __status__ = 'Development'
+__copyright__ = 'Copyright 2015, GISi'
+__license__ = 'MIT'
 
 from arcpylogger import ArcpyMessageStream
 from arcpylogger import ArcpyMessageHandler
