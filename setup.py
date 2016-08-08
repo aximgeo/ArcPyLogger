251b554ad1a0bc81d99ac2b5d9c1eb13a186f605
Charles Arnold <charles.arnold@gisinc.com>
2016-08-08 16:52:45 -0700

    bump version
    bumping major because 64bacf814ce208565 is potentially a breaking change (i.e., it can break users who haven't yet switched to using `.pyt` toolboxes)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='arcpylogger',
-    version='1.0.0',
+    version='2.0.0',
     packages=['arcpylogger'],
     url='https://github.com/gisinc/ArcPyLogger',
     license='MIT',
